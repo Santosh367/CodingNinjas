7818755a8bb3b55871375c26546a74042c02aa6b
Santosh Kumar Dash <santoshdash367@gmail.com>
2022-11-28 05:04:03 +0530

    Create CheckArrayRotation.java

---
+++ CodingNinja Solutions/Arrays/src/CheckArrayRotation.java
@@ -0,0 +1,12 @@
+public class CheckArrayRotation {
+    public static int arrayRotateCheck(int[] arr){
+
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                return i+1;
+            }
+
+        }
+        return 0;
+    }
+}
